f9f2a84e0a4e7e7c1fac3bcb1db2b7406798f63e
TheLazyCowboy1 <Nathan@331oak.com>
2026-02-16 21:39:26 -0500

    fixed config slider

--- Options.cs
+++ Options.cs
@@ -6,7 +6,7 @@ public class Options : OptionInterface
 {
     public Options() : base()
     {
-        DestructionLevel = this.config.Bind<float>("DestructionLevel", 30, new ConfigAcceptableRange<float>(0, 100));
+        DestructionLevel = this.config.Bind<float>("DestructionLevel", 20, new ConfigAcceptableRange<float>(0, 100));
     }
 
     public Configurable<float> DestructionLevel;
@@ -19,12 +19,14 @@ public class Options : OptionInterface
         this.Tabs = new OpTab[] { tab };
 
         tab.AddItems(
-            new OpFloatSlider(DestructionLevel, new(30, 300), 300, 1, false) { description = "How strong the destruction effect is. There is no strict metric for this. Just know that 60 is twice as strong as 30." },
-            new OpLabel(100, 350, "Destruction Level"),
-            new OpLabel(30, 280, "0%") { alignment = FLabelAlignment.Center },
-            new OpLabel(130, 280, "25%") { alignment = FLabelAlignment.Center },
-            new OpLabel(230, 280, "50%") { alignment = FLabelAlignment.Center },
-            new OpLabel(330, 280, "100%") { alignment = FLabelAlignment.Center }
+            //new OpSlider(DestructionLevel, new(50, 300), 480) { description = "How strong the destruction effect is. There is no strict metric for this. Just know that 60 is twice as strong as 30." },
+            new OpFloatSlider(DestructionLevel, new(50, 300), 480, 1, false) { Increment = 1, description = "How strong the destruction effect is. There is no strict metric for this. Just know that 60 is twice as strong as 30." },
+            new OpLabel(200, 350, "Destruction Level"),
+            new OpLabel(40, 280, "0%"),
+            new OpLabel(160, 280, "25%"),
+            new OpLabel(280, 280, "50%"),
+            new OpLabel(400, 280, "50%"),
+            new OpLabel(520, 280, "100%")
             );
 
     }
